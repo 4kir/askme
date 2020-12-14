ddd5861520a185d378ee77b28cc204948b90b213
4kir <tkakenov@mail.ru>
2020-12-15 00:15:42 +0600

    add author to question

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,4 @@ Rails.application.routes.draw do
   get 'sign_up' => 'users#new'
   get 'log_out' => 'sessions#destroy'
   get 'log_in' => 'sessions#new'
-
 end
---
+++ db/migrate/20201213093249_add_author_to_question.rb
@@ -0,0 +1,7 @@
+class AddAuthorToQuestion < ActiveRecord::Migration[6.0]
+  def change
+    add_column :questions, :author_id, :integer
+
+    add_index :questions, :author_id
+  end
+end

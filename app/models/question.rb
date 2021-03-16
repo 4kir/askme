31b26a8cb3d06363f4707138aa2ea50e90c58476
4kir <tkakenov@mail.ru>
2021-03-17 03:07:02 +0600

    Add Hashtags to main pages

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   has_many :hashtag_questions, dependent: :destroy
-  has_many :hashtaq, through: :hashtag_questions
+  has_many :hashtags, through: :hashtag_questions
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
 
